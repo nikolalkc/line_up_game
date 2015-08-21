6cfb1c4bacd4e07433258835c4f0bb15b133a3fd
Nikola Lukic <nikolalkc@hotmail.com>
2015-08-21 17:19:05 +0200

    Real Ball Ubacen i pada po linijama

---
+++ Assets/MainControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainControl : MonoBehaviour {
+	public GameObject real_ball;
+	public GameObject ball_position;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (2)) {
+			GameObject ball = Instantiate(real_ball) as GameObject;
+			Vector3 starting_ball_position = ball_position.transform.position;
+			ball.transform.position = starting_ball_position + new Vector3 (0.5f,0f,0f);
+		}
+	}
+}
